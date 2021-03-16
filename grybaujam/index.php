7c2276bcf1a8aa72d2acc9892d81196af7920cd6
Ana Višnevskaja <anavisnia@gmail.com>
2021-03-16 13:01:43 +0200

    autoload

--- grybaujam/index.php
+++ grybaujam/index.php
@@ -1,6 +1,37 @@
 <?php
-include 'Krepsys.php';
-include 'Grybas.php';
+// include 'Krepsys.php';
+// include 'Grybas.php';
+include 'vendor\autoload.php';
+
+// spl_autoload_register(function ($class) {
+
+//     $prefix = 'Miskas\Didelis';
+//     $base_dir = __DIR__ . '/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
+
+// spl_autoload_register(function ($class) {
+
+//     $prefix = '';
+//     $base_dir = __DIR__ . '/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
 
 $krepsys1 = new Miskas\Didelis\Krepsys;
 
